1efb2b7d2ff5fe4eb70373e2f67e3bda24e28898
Ernur Torekul <ernurtorekul@gmail.com>
2024-11-10 20:04:23 +0500

    intro

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Home">
+    <h1 class="text-2xl font-bold">TRAcKify</h1>
+
+    <button @click="showMessage">button</button>
+    <p v-if="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {
+            message: null,
+        };
+    },
+    methods:{
+        showMessage(){
+            this.message = "This is a message from the Trackify component";
+        },
+    },
+};
+</script>
